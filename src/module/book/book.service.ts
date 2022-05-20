02811d02d7410bcad256319d29c1857e83d0886e
JMustang <eecfredes@gmail.com>
2022-05-20 17:38:11 -0300

    Database - PrismaService

--- src/module/book/book.service.ts
+++ src/module/book/book.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/PrismaService';
 import { BookDto } from './book.dto';
 
 @Injectable()
 export class BookService {
+  constructor(private prisma: PrismaService) {
+    // empty
+  }
+
   async create(data: BookDto) {
-    // ...
+    const bookExists = await this.prisma.book.findFirst({
+      where: {
+        bar_code: data.bar_code,
+      },
+    });
+
+    if (bookExists) {
+      throw new Error('Book already exists');
+    }
+
+    const book = await this.prisma.book.create({
+      data,
+    });
+    return book;
   }
 }
