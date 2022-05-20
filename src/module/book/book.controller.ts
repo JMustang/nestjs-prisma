597913e7e65b217267a023fe6da6e754a12f245c
JMustang <eecfredes@gmail.com>
2022-05-20 17:42:14 -0300

    Controller -@Post Books

--- src/module/book/book.controller.ts
+++ src/module/book/book.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { BookDto } from './book.dto';
 import { BookService } from './book.service';
 
 @Controller('book')
 export class BookController {
-  constructor(private readonly bookService: BookService) {}
+  constructor(private readonly bookService: BookService) {
+    // empty
+  }
+
+  @Post()
+  async create(@Body() data: BookDto) {
+    return this.bookService.create(data);
+  }
 }
